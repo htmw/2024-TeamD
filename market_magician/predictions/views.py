6a58e742a4d716356f9421d53e0ff344e3a20956
shady <henrykim89@gmail.com>
2024-10-14 22:35:04 -0400

    Update views.py with experimental index page Updated requirements.txt with latest library additions Added text describing python version for venv

--- market_magician/predictions/views.py
+++ market_magician/predictions/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import tensorflow as tf
 
 
 # landing page
 def index(request):
-    template = loader.get_template("index.html")
-    return HttpResponse(template.render())
+    data_df = yf.download("GOOG", period='1mo') 
+    data_html = data_df.to_html()
+
+    return HttpResponse(data_html)
