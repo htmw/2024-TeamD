bc97d3f803f1100029a544770c940843232e12c4
Kassandra Camarillo <kassiecam@gmail.com>
2024-12-15 09:48:45 -0800

    Final css

--- market_magician_dev/frontend/src/App.js
+++ market_magician_dev/frontend/src/App.js
@@ -7,25 +7,26 @@ import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import BuyStocks from './pages/BuyStocks';
 import SearchTicker from './pages/SearchTicker';
-
 import './App.css';
-
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/buyStocks" element={<BuyStocks />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/searchTicker" element={<SearchTicker />} />
-      </Routes>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/buyStocks" element={<BuyStocks />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/searchTicker" element={<SearchTicker />} />
+        </Routes>
+      </main>
       <Footer />
     </div>
   );
 }
 
 export default App;
+
 
